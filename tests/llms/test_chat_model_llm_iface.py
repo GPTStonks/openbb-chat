bbfed96eb7ee96a61fd421f7da574a88a2bb8ea6
Daedalus <daedalus314@protonmail.com>
2023-12-29 23:29:50 +0100

    Add ChatModelWithLLMIface
    ChatModelWithLLMIface is a wrapper around langchain's chat models to be
    used with a LLM interface. It is useful for instance with ChatOpenAI, as
    `gpt-3.5-turbo-1106` only supports the chat interface in langchain.

---
+++ tests/llms/test_chat_model_llm_iface.py
@@ -0,0 +1,17 @@
+import os
+from collections import namedtuple
+from unittest.mock import patch
+
+import pytest
+from langchain.chat_models import ChatOpenAI
+
+from openbb_chat.llms.chat_model_llm_iface import ChatModelWithLLMIface
+
+
+@patch.object(ChatOpenAI, "_generate", return_value=namedtuple("output", "generations")([]))
+def test_chat_model_llm_iface(mocked_generate):
+    os.environ["OPENAI_API_KEY"] = "randomkeyfortesting"
+    chat = ChatModelWithLLMIface(chat_model=ChatOpenAI(temperature=0))
+    chat._generate(["Generate some random story"])
+
+    mocked_generate.assert_called_once()
